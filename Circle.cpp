a78d77df1267e63e3687dbcb1d07e2669ff9d375
gabrielcasapacheco391-maker <gabrielcasapacheco391@gmail.com>
2026-09-07 11:08:39 -0300

    Update Circle.cpp

--- Circle.cpp
+++ Circle.cpp
@@ -1,11 +1,16 @@
 #include "Circle.h"
 
-Circle::Circle(std::string n,double r):Shape(n)
-{
- radius=r;
+
+
+Circle::Circle(std::string n, double r) : Shape(n) {
+    radius = r;
 }
 
-Circle::~Circle()
-{
-    //dtor
+// Destrutor
+Circle::~Circle() {
+}
+
+// Implementação do cálculo da área (Pi * Raio ao quadrado)
+double Circle::calcularArea() {
+    return 3.14159 * std::pow(radius, 2);
 }
